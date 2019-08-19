45f8e1bbaacec1c429ac37f7aea30e9ed3f58241
piaoge <2393764821@qq.com>
2019-08-19 21:31:14 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
