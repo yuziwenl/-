47b2c3c55064f88e0c0026c8bdfd7b6e51048823
piaoge <2393764821@qq.com>
2019-08-19 21:26:24 +0800

    数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
           <div class="title">
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
-          <el-form>
-              <el-form-item>
-                  <el-input placeholder='请输入手机号'></el-input>
+          <el-form :model='formData' :rules='rules' ref='loginForm'>
+              <el-form-item prop='mobile'>
+                  <el-input v-model='formData.mobile' placeholder='请输入手机号'></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input style='width:67%' placeholder='请输入验证码'></el-input>
+              <el-form-item prop='code'>
+                  <el-input  v-model='formData.code' style='width:67%' placeholder='请输入验证码'></el-input>
                   <el-button style="float:right">发送验证码</el-button>
               </el-form-item>
-            <el-form-item>
-                <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+            <el-form-item prop='check'>
+                <el-checkbox  v-model='formData.check' >我已阅读并同意用户协议和隐私条款</el-checkbox>
             </el-form-item>
             <el-form-item>
-                <el-button type='primary' style='width:100%'>登录</el-button>
+                <el-button @click='login' type='primary' style='width:100%'>登录</el-button>
             </el-form-item>
           </el-form>
       </el-card>
@@ -25,7 +25,41 @@
 
 <script>
 export default {
-
+  data () {
+    var func = function (rule, value, callback) {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error('您必须同意协议'))
+      }
+    }
+    return {
+      formData: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '登录手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '验证码必须为6位数字' }
+        ],
+        check: [{
+        //   message: '您必须无条件被坑',
+          validator: func
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate()
+    }
+  }
 }
 </script>
 
