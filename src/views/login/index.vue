6771f07d94e372a7f478968eca8e03119d901c01
piaoge <2393764821@qq.com>
2019-08-20 19:20:57 +0800

    登录成功及失败处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -67,6 +67,11 @@ export default {
             // console.log(result.data.data)
             window.localStorage.setItem('user-info', JSON.stringify(result.data.data))
             this.$router.push('/home')
+          }).catch(() => {
+            this.$message({
+              message: '当前用户名或者密码错误',
+              type: 'warning'
+            })
           })
         }
       })
