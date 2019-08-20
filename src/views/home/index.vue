912bfaa968a031c55a1d43ee8862b9f0d857a286
piaoge <2393764821@qq.com>
2019-08-20 19:24:50 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home">
-    home
-  </div>
+<el-container>
+  <el-aside>左侧内容</el-aside>
+  <el-container>
+    <el-header>头部区域</el-header>
+    <el-main>中间区域</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
