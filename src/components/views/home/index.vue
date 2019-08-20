38267a631d5b2cb74fdee9083497787089b043d8
piaoge <2393764821@qq.com>
2019-08-20 21:11:04 +0800

    主页模块-头部结构及下拉菜单

--- src/components/views/home/index.vue
+++ src/components/views/home/index.vue
@@ -4,7 +4,9 @@
     <layout-aside></layout-aside>
   </el-aside>
   <el-container>
-    <el-header>头部区域</el-header>
+    <el-header>
+      <layout-header></layout-header>
+    </el-header>
     <el-main>中间区域</el-main>
   </el-container>
 </el-container>
@@ -12,9 +14,11 @@
 
 <script>
 import layoutAside from '@/components/views/home/layout-aside'
+import layoutHeader from '@/components/views/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
---
+++ src/components/views/home/layout-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row style='padding:12px 0' type="flex" justify="space-between">
+    <el-col :span="18" style='display:flex;align-items:center'>
+      <i style='font-size:20px;margin-right:4px' class="el-icon-s-unfold"></i>
+      <span>江苏传智播客教育有限股份公司</span>
+    </el-col>
+    <el-col :span="3" style="display">
+        <img style='width:40px;height:40px;border-radius:50%;margin-right:10px;align-items:center' src="@/assets/img/avatar.jpg" alt="">
+         <el-dropdown trigger="click">
+      <span class="el-dropdown-link">
+        我是56<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item >个人信息</el-dropdown-item>
+        <el-dropdown-item >git地址</el-dropdown-item>
+        <el-dropdown-item >退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
