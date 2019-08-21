62dd20c2136fb106048214d50d09167bee1d7171
piaoge <2393764821@qq.com>
2019-08-21 17:18:42 +0800

    黑马头条PC-主页模块-导航守卫

---
+++ src/permission.js
@@ -0,0 +1,19 @@
+import router from './router'
+router.beforeEach(function (to, from, next) {
+  if (to.path.startsWith('/home')) {
+    let result = window.localStorage.getItem('user-info')
+    if (result) {
+      let userInfo = JSON.parse(result)
+      if (userInfo && userInfo.token) {
+        next()
+      } else {
+        next('/login')
+      }
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
+export default router
