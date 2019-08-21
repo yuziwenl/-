974d030d1bb3e1797ec1640c3137350c86675846
piaoge <2393764821@qq.com>
2019-08-21 16:19:09 +0800

    主页模块-头部信息查询

--- src/components/views/home/layout-header.vue
+++ src/components/views/home/layout-header.vue
@@ -1,27 +1,65 @@
 <template>
-  <el-row style='padding:12px 0' type="flex" justify="space-between">
-    <el-col :span="18" style='display:flex;align-items:center'>
-      <i style='font-size:20px;margin-right:4px' class="el-icon-s-unfold"></i>
+  <el-row style="padding:12px 0" type="flex" justify="space-between">
+    <el-col :span="18" style="display:flex;align-items:center">
+      <i style="font-size:20px;margin-right:4px" class="el-icon-s-unfold"></i>
       <span>江苏传智播客教育有限股份公司</span>
     </el-col>
     <el-col :span="3" style="display">
-        <img style='width:40px;height:40px;border-radius:50%;margin-right:10px;align-items:center' src="@/assets/img/avatar.jpg" alt="">
-         <el-dropdown trigger="click">
-      <span class="el-dropdown-link">
-        我是56<i class="el-icon-arrow-down el-icon--right"></i>
-      </span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item >个人信息</el-dropdown-item>
-        <el-dropdown-item >git地址</el-dropdown-item>
-        <el-dropdown-item >退出</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
+      <img
+        style="width:40px;height:40px;border-radius:50%;margin-right:10px;align-items:center"
+        :src="!user.photo?user.photo:defaultImg"
+        alt
+      />
+      <el-dropdown trigger="click" @command='commandAction'>
+        <span class="el-dropdown-link">
+          {{user.name}}
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command='account'>个人信息</el-dropdown-item>
+          <el-dropdown-item command='git'>git地址</el-dropdown-item>
+          <el-dropdown-item command='end'>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </el-col>
   </el-row>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      user: {},
+      defaultImg: require('@/assets/img/avatar.jpg')
+    }
+  },
+  methods: {
+    getUesrInfo () {
+      let userInfo = window.localStorage.getItem('user-info')
+      let token = userInfo ? JSON.parse(userInfo).token : null
+      token &&
+        this.$axios({
+          url: '/user/profile',
+          headers: { Authorization: `Bearer  ${token}` }
+        }).then(result => {
+          this.user = result.data.data
+        })
+    },
+    commandAction (command) {
+      if (command === 'account') {
+
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/yuziwenl/-'
+      } else {
+        window.localStorage.clear()
+        this.$router.push('/login')
+      }
+    }
+  },
+  created () {
+    this.getUesrInfo()
+  }
+}
 </script>
 
 <style>
