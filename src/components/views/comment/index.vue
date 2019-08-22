910d96664a53a0b84732544ae93a1e7e13193eef
piaoge <2393764821@qq.com>
2019-08-22 21:06:44 +0800

    打开和关闭评论

--- src/components/views/comment/index.vue
+++ src/components/views/comment/index.vue
@@ -4,11 +4,14 @@
       <template slot="title">评论列表</template>
     </bread-crumb>
     <el-table :data="list">
-      <el-table-column  label="标题" width="500"></el-table-column>
-      <el-table-column  label="评论状态"></el-table-column>
-      <el-table-column  label="总评论数" ></el-table-column>
-      <el-table-column  label="粉丝评论数" ></el-table-column>
-      <el-table-column  label="操作" ></el-table-column>
+      <el-table-column prop="title" label="标题" width="500"></el-table-column>
+      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数" ></el-table-column>
+      <el-table-column  prop="fans_comment_count" label="粉丝评论数" ></el-table-column>
+      <el-table-column  label="操作" >
+      <el-button type='text'>修改</el-button>
+      <el-button type='text'>关闭评论</el-button>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -19,6 +22,24 @@ export default {
     return {
       list: []
     }
+  },
+  methods: {
+    getComments () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatter (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComments()
   }
 }
 </script>
