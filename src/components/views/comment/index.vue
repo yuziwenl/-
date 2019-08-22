53d32242205290453f2be5a841379a12f0f39496
piaoge <2393764821@qq.com>
2019-08-22 20:49:57 +0800

    页面table使用

--- src/components/views/comment/index.vue
+++ src/components/views/comment/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>评论</div>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="list">
+      <el-table-column  label="标题" width="500"></el-table-column>
+      <el-table-column  label="评论状态"></el-table-column>
+      <el-table-column  label="总评论数" ></el-table-column>
+      <el-table-column  label="粉丝评论数" ></el-table-column>
+      <el-table-column  label="操作" ></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
