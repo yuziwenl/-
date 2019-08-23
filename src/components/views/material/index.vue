619af66a6bff54317c0d4bd9567205c7e81d2848
piaoge <2393764821@qq.com>
2019-08-23 21:50:01 +0800

    素材列表-数据加载

--- src/components/views/material/index.vue
+++ src/components/views/material/index.vue
@@ -1,29 +1,86 @@
 <template>
-<el-card>
-<bread-crumb slot="header">
-<template slot="title">
-素材管理
-</template>
-</bread-crumb>
-<el-tabs v-model="activeName">
-    <el-tab-pane label="全部图片" @tab-click='changeTab' name="all">
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs v-model="activeName" @tab-click='changeTab'>
+      <el-tab-pane label="全部图片" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+             <el-row align="middle" type="flex" justify="space-around" class="operate">
+              <i class="el-icon-star-off"></i>
+              <i class="el-icon-delete"></i>
+            </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
 
-        </el-tab-pane>
-    <el-tab-pane label="收藏图片" name="second"></el-tab-pane>
-  </el-tabs>
+         <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+             <el-row align="middle" type="flex" justify="space-around" class="operate">
+            </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+
+    </el-tabs>
   </el-card>
 </template>
-
 <script>
 export default {
   data () {
     return {
-      activeName: 'all'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    changeTab () {
+      this.getMaterial()
+    },
+    getMaterial () {
+      this.$axios({
+        url: 'user/images',
+        params: { collect: this.activeName === 'collect' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.img-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 200px;
+    margin: 20px 10px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 30px;
+      width: 100%;
+       background-color: #f4f5f6;
+      i {
+        font-size: 24px;
+      }
+    }
+  }
+}
 </style>
